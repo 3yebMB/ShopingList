96d7727cd8739f2a6b403e5c25168469406d3f35
Michael <45770882+3yebMB@users.noreply.github.com>
2021-11-08 20:06:06 +0500

    Revert "add shop list adapter"

--- app/src/main/java/dev/m13d/shopinglist/domain/AddShopItemInteractor.kt
+++
@@ -1,8 +0,0 @@
-package dev.m13d.shopinglist.domain
-
-class AddShopItemInteractor(private val shopListRepository: ShopListRepository) {
-
-    fun addShopItem(shopItem: ShopItem) {
-        shopListRepository.addShopItem(shopItem)
-    }
-}
--- app/src/main/java/dev/m13d/shopinglist/domain/EditShopItemInteractor.kt
+++
@@ -1,8 +0,0 @@
-package dev.m13d.shopinglist.domain
-
-class EditShopItemInteractor(private val shopListRepository: ShopListRepository) {
-
-    fun editShopItem(shopItem: ShopItem) {
-        shopListRepository.editShopItem(shopItem)
-    }
-}
