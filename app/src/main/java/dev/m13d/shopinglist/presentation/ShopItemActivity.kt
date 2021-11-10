8a3c29c8df64d793e5103f059999e87e177b5306
m.zuev <m.zuev@azoft.com>
2021-11-10 21:59:03 +0100

    add activity for shop item details

---
+++ app/src/main/java/dev/m13d/shopinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package dev.m13d.shopinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.m13d.shopinglist.R
+
+class ShopItemActivity : AppCompatActivity(R.layout.activity_shop_item) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_shop_item)
+    }
+}
