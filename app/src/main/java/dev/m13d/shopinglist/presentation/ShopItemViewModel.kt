3708f987759228367d51e7210fe525556fbb56f6
m.zuev <m.zuev@azoft.com>
2021-11-10 23:10:17 +0100

    add livedata

--- app/src/main/java/dev/m13d/shopinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/dev/m13d/shopinglist/presentation/ShopItemViewModel.kt
@@ -1,5 +1,7 @@
 package dev.m13d.shopinglist.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dev.m13d.shopinglist.data.ShopListRepositoryImpl
 import dev.m13d.shopinglist.domain.AddShopItemInteractor
@@ -16,8 +18,25 @@ class ShopItemViewModel : ViewModel() {
     private val editShopItemInteractor = EditShopItemInteractor(repository)
     private val addShopItemInteractor = AddShopItemInteractor(repository)
 
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
+
     fun getShopItem(shopItemId: Int) {
         val item = getShopItemInteractor.getShopItem(shopItemId)
+        _shopItem.value = item
     }
 
     fun addShopItem(txtName: String?, txtCount: String?) {
@@ -27,6 +46,7 @@ class ShopItemViewModel : ViewModel() {
         if (inputValid) {
             val shopItem = ShopItem(name, count, true)
             addShopItemInteractor.addShopItem(shopItem)
+            finishWork()
         }
     }
 
@@ -35,8 +55,11 @@ class ShopItemViewModel : ViewModel() {
         val count = parseCount(txtCount)
         val inputValid = validateInput(name, count)
         if (inputValid) {
-            val shopItem = ShopItem(name, count, true)
-            editShopItemInteractor.editShopItem(shopItem)
+            _shopItem.value?.let {
+                val item = it.copy(name = name, count = count)
+                editShopItemInteractor.editShopItem(item)
+                finishWork()
+            }
         }
     }
 
@@ -55,14 +78,26 @@ class ShopItemViewModel : ViewModel() {
     private fun validateInput(name: String, count: Int): Boolean {
         var result = true
         if (name.isBlank()) {
-            // TODO: show error input name
+            _errorInputName.value = true
             result = false
         }
         if (count <= 0) {
-            // TODO: show error input count
+            _errorInputCount.value = true
             result = false
         }
         return result
+    }
+
+    fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    fun resetErrorInputCount() {
+        _errorInputCount.value = false
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
     }
 }
 
